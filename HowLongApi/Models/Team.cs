8460afd91c3ae35d6e8dd7faac05446b2fb23ab8
vitoladev <asaptorvi@protonmail.com>
2021-01-24 00:40:49 -0300

    Add project files.

---
+++ HowLongApi/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using HowLongApi.Models.Enums;
+
+namespace HowLongApi.Models
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public Sport Sport { get; set; }
+        public string Championship { get; set; }
+        public DateTime ChampionshipDate { get; set; }
+    }
+}
